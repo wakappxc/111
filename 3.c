4ea0e95daee60df55fccf2824246b26b65362d5c
wakappxc <1119650965@qq.com>
2025-11-21 09:28:34 +0800

    添加新文件 3.c，包含打印“Hello, World!”的示例代码

---
+++ 3.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+    return 11;
+
+
+    w s 我
+}#include "stdio.h"
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+    return 11;
+
+
+    w s 我
+}
